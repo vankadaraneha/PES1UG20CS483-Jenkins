47eec55469a834e08bf1968e72946baaea7a40f5
Neha Vankadara <84381950+vankadaraneha@users.noreply.github.com>
2023-02-17 11:55:04 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, I am neha\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
